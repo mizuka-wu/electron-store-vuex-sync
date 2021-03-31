f4156f134e45dd12d023e01cef88715257cd094c
水华 <yiwei.wu@mihoyo.com>
2021-03-31 19:33:14 +0800

    feat: build

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,40 @@
+import commonjs from 'rollup-plugin-commonjs'
+import resolve from 'rollup-plugin-node-resolve'
+
+const plugins = [
+  resolve({
+    jsnext: true,
+    main: true,
+    browser: true
+  }),
+  commonjs()
+]
+
 export default [
   {
     input: 'src/main.js',
     output: {
       file: 'dist/bundle.js',
-      format: 'umd'
-    }
+      format: 'umd',
+      name: 'electron-store-vuex-sync'
+    },
+    plugins
   },
   {
     input: 'src/background.js',
     output: {
       file: 'dist/background.js',
-      format: 'umd'
+      format: 'umd',
+      name: 'injectStore'
     }
   },
   {
     input: 'src/vuex.js',
     output: {
       file: 'dist/vuex.js',
-      format: 'umd'
-    }
+      format: 'umd',
+      name: 'getVuexStoreConfig'
+    },
+    plugins
   }
 ]
